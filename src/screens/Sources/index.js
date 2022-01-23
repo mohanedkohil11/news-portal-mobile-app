d67bf7705d2e9887913b41703dd8cf5a6710de96
Mohaned Kohil <moha.kohil@gmail.com>
2022-01-23 19:34:48 +0200

    add screen to list all sources (sources screen)

--- src/screens/Sources/index.js
+++ src/screens/Sources/index.js
@@ -1,6 +1,45 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { FlatList, RefreshControl, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTopHeadlinesSources } from '../../redux/actions/news';
+import styles from './styles';
+import SourceCard from '../../components/SourceCard';
 
-export default function Sources() {
-    return <Text>Sources</Text>;
+export default function Sources({ navigation }) {
+    const dispatch = useDispatch();
+    const [refreshing, setRefreshing] = useState(false); // refresh state
+    const { topHeadlinesSources } = useSelector(state => state.news);
+
+    const loadData = useCallback(() => {
+        // call to load top headlines sources
+        setRefreshing(true);
+        dispatch(getTopHeadlinesSources(() => setRefreshing(false)));
+    }, [dispatch]);
+
+    const onCardPress = source => {
+        // navigate to source news screen with source name as params
+        navigation.navigate('SourceNews', source);
+    };
+
+    useEffect(() => {
+        //load data in initial load & on filters change
+        loadData();
+    }, [loadData]);
+
+    return (
+        <View style={styles.flexOne}>
+            <SafeAreaView style={styles.flexOne}>
+                <FlatList
+                    refreshControl={<RefreshControl refreshing={refreshing} onRefresh={loadData} />}
+                    data={topHeadlinesSources}
+                    initialNumToRender={10}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => {
+                        return <SourceCard source={item.name} onPress={() => onCardPress(item)} />;
+                    }}
+                />
+            </SafeAreaView>
+        </View>
+    );
 }
