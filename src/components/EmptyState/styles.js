6ad6b2e12d242d9d411130aaa2646ea11bb92266
Mohaned Kohil <moha.kohil@gmail.com>
2022-01-24 11:54:18 +0200

    add empty state component

---
+++ src/components/EmptyState/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+import { BaseColor } from '../../config';
+
+export default StyleSheet.create({
+    container: {
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        color: BaseColor.text,
+    },
+});
